73c31e907258e129ae6a7f56e17929c110bdc74b
kooo0223 <yamakou0223@gmail.com>
2021-10-01 19:30:11 +0900

    ユーザーモデルのバリデーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :nickname, presence: true
+  validates :email, format: {with: /\A\S+@\S+\.\S+\z/}, presence: true
+  validates :password, format: {with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i}, presence: true
+  validates :last_name, format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/}, presence: true
+  validates :first_name, format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/}, presence: true
+  validates :last_name_kana, format: {with: /\A[ァ-ヶ一-]+\z/}, presence: true
+  validates :first_name_kana, format: {with: /\A[ァ-ヶ一-]+\z/}, presence: true
+  validates :birth_date, presence: true
 end
