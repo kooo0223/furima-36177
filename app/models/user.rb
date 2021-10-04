f763afa1583e236ff05045dbb3ee94f0bd7a65ad
kooo0223 <yamakou0223@gmail.com>
2021-10-04 20:43:20 +0900

    ユーザー管理機能_修正_4

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ApplicationRecord
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
   validates_format_of :password, with: PASSWORD_REGEX, message: 'は半角英数字混合で設定してください'
 
-  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶー-龥々]+\z/, message: 'は全角文字を使用してください' } do
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶー-龥々ー]+\z/, message: 'は全角文字を使用してください' } do
     validates :last_name
     validates :first_name
   end
