049943481a2249a2c6c2ede93b45d6f21fb58328
kooo0223 <yamakou0223@gmail.com>
2021-10-05 17:03:25 +0900

    Active Storageの導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,10 +2,21 @@ class ItemsController < ApplicationController
   before_action :move_to_index, except: [:index, :show]
 
   def index
+    @item = Item.all
+  end
+
+  def new
+    @item = Item.new
   end
 
   #def show
   #end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:text, :image).merge(user_id: current_user.id)
+  end
 
   #def move_to_index
   #  redirect_to action: :index unless user_signed_in?
